9b413035933384e79afeb93bcc6433356332f6f4
ravuthz <ravuthz@gmail.com>
2021-12-30 21:33:26 +0700

    Deploy Github Page

--- scripts/deploy-github-pages.sh
+++ scripts/deploy-github-pages.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+git checkout gh-pages
+
 npm run export
 
 touch out/.nojekyll
 
 git add -f out
 
-git commit -m "Deploy Github Page at `date +'%Y-%m-%d %H:%M:%S'`"
+git commit -m "Deploy Github Page"
 
-# git push -f origin gh-pages
+git push -f origin gh-pages
 
-git subtree push --prefix out origin gh-pages
+# git commit -m "Deploy Github Page at `date +'%Y-%m-%d %H:%M:%S'`"
+
+# git subtree push --prefix out origin gh-pages
