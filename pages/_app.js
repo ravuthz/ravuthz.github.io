51167144a5be785042ff15a9435f6ea8c75d6c53
ravuthz <ravuthz@gmail.com>
2022-02-17 13:11:34 +0700

    Add meta version for build version

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '@/css/prism.css'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
+import moment from 'moment'
 
 import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
@@ -12,11 +13,15 @@ import { ClientReload } from '@/components/ClientReload'
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
+const { version } = require('../package.json')
+const build = moment().format('YYYYMMDDHHmmss')
+
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="version" content={version + '.' + build} />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
