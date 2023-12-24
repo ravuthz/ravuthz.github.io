e821f92595a668d43d9800e3ee1ed18b7cd1bb32
Ravuthz <ravuthz@gmail.com>
2023-12-24 22:37:23 +0700

    Add partytown react solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
-import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    partytown(),
+    react({ include: ["**/react/*"] }),
+    solidJs({ include: ["**/solid/*"] }),
+  ],
 });
