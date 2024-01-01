de8fd62550ff5a33e2e170a77fc67c4c2bfdf141
Ravuthz <ravuthz@gmail.com>
2024-01-02 00:29:28 +0700

    Updates

--- server/chunks/prerender_882c2vnO.mjs
+++
@@ -1,3 +0,0 @@
-const noop = () => {};
-export const i = noop;
-export const r = noop;
--- server/chunks/rss_YYVICCwl.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./prerender_882c2vnO.mjs').then(n => n.r);
-
-export { page };
